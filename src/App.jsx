adcae51c93e08d88f02b63c53b74d3620e93a065
Raj Jaiswal <rajj91783@gmail.com>
2024-09-27 19:28:15 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs text-blue-500 text-3xl">
-        Click on the Vite and React logos to learn more
+        Pagal ho ke nacho ya naach ke pagal ho jao
       </p>
     </>
   );
